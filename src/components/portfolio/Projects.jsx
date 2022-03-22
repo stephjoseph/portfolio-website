c5057f46593efdf2dc9e672de252ed0d2885b443
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-22 22:05:28 +0800

    Update Projects.jsx

--- src/components/portfolio/Projects.jsx
+++ src/components/portfolio/Projects.jsx
@@ -1,5 +1,27 @@
+import Tip from "./Tip";
+import Officelite from "./Officelite";
+import Maker from "./Maker";
+import Typemaster from "./Typemaster";
+import Github from "./Github";
+import Art from "./Art";
+import Suite from "./Suite";
+import Time from "./Time";
+import Crowdfunding from "./Crowdfunding";
+
 const Projects = () => {
-  return <section className="flex flex-col gap-[4.5rem] md:gap-20"></section>;
+  return (
+    <section className="flex flex-col gap-[4.5rem] md:gap-20">
+      <Tip />
+      <Officelite />
+      <Maker />
+      <Typemaster />
+      <Github />
+      <Art />
+      <Suite />
+      <Time />
+      <Crowdfunding />
+    </section>
+  );
 };
 
 export default Projects;
