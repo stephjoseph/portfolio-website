23448b5097cda5ab7467b948d2ed40c3c33fa214
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-22 23:48:20 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import MobileNav from "./components/MobileNav";
 import Navbar from "./components/Navbar";
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ScrollToTop from "./components/scrollToTop";
 
 function App() {
   const [size, setSize] = useState({
@@ -22,6 +23,7 @@ function App() {
   useEffect(() => (window.onresize = updateSize), []);
   return (
     <Router>
+      <ScrollToTop />
       <div id="App" className="grid place-items-center bg-[#fafafa]">
         <div className="relative w-full max-w-[40rem] bg-[#fafafa] px-8 md:max-w-[64rem] md:px-[2.438rem] xl:max-w-[96rem] xl:px-[10.313rem]">
           {size.x < 768 && <MobileNav />}
