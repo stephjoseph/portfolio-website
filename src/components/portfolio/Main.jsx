06d1de441e4d5ea0750c5304f3d83e926211e191
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-22 22:05:21 +0800

    Update Main.jsx

--- src/components/portfolio/Main.jsx
+++ src/components/portfolio/Main.jsx
@@ -3,7 +3,7 @@ import CTA from "../CTA";
 
 const Main = () => {
   return (
-    <main>
+    <main className="flex w-full max-w-[40rem] flex-col gap-[7.188rem] md:mt-[2.938rem] md:max-w-[64rem] md:gap-24 xl:mt-10 xl:max-w-[96rem] xl:gap-[9.375rem]">
       <Projects />
       <CTA />
     </main>
