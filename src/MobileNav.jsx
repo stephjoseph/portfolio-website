e248c34e12263b639468613afe4fc5babddd4063
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-22 11:34:17 +0800

    Update MobileNav.jsx

--- src/MobileNav.jsx
+++ src/MobileNav.jsx
@@ -1,17 +1,53 @@
-import logo from "./src/logo.svg";
+import logo from "./logo.svg";
+import iconHamburger from "./images/icons/hamburger.svg";
+import iconClose from "./images/icons/close.svg";
+import React, { useState } from "react";
 
 const MobileNav = () => {
+  const [isActive, setisActive] = useState(false);
+
+  function handleClick() {
+    setisActive((prevState) => !prevState);
+  }
   return (
-    <div>
-      <nav
-        className="inactive-nav absolute top-[5.5rem] right-8 z-10 flex h-[11.625rem] w-[13.938rem] items-center justify-center bg-[#33323D] transition-opacity duration-300 ease-in md:hidden"
+    <nav>
+      <div className="relative mt-8 mb-10 flex w-full items-center justify-between md:hidden">
+        <a className="w-[3.755rem]" href="/">
+          <img src={logo} alt="logo" />
+        </a>
+        <button
+          className={`${
+            isActive ? "inactive-button" : ""
+          } absolute top-[0.625rem] right-0 transition-[transform,_opacity] duration-300 ease-in`}
+          id="openNav"
+          type="button"
+          onClick={handleClick}
+        >
+          <img src={iconHamburger} alt="" />
+        </button>
+        <button
+          className={`${
+            !isActive ? "inactive-button" : ""
+          } absolute top-[0.438rem] right-[0.188rem] transition-[transform,_opacity] duration-300 ease-in`}
+          id="closeNav"
+          type="button"
+          onClick={handleClick}
+        >
+          <img src={iconClose} alt="" />
+        </button>
+      </div>
+
+      <div
+        className={`${
+          !isActive ? "inactive-nav" : ""
+        } absolute top-[5.5rem] right-8 z-10 flex h-[11.625rem] w-[13.938rem] items-center justify-center bg-[#33323D] transition-opacity duration-300 ease-in md:hidden`}
         id="mobile-nav"
       >
         <ul className="flex h-[6.625rem] w-28 flex-col items-center justify-between gap-0 text-center">
           <li>
             <a
               className="text-[0.75rem] font-normal uppercase leading-[0.875rem] tracking-[2px] text-white"
-              href="./index.html"
+              href="/"
             >
               Home
             </a>
@@ -19,7 +55,7 @@ const MobileNav = () => {
           <li>
             <a
               className="text-[0.75rem] font-normal uppercase leading-[0.875rem] tracking-[2px] text-white"
-              href="./portfolio.html"
+              href="/portfolio"
             >
               Portfolio
             </a>
@@ -27,33 +63,14 @@ const MobileNav = () => {
           <li>
             <a
               className="text-[0.75rem] font-normal uppercase leading-[0.875rem] tracking-[2px] text-white"
-              href="./contact.html"
+              href="/contact"
             >
               Contact Me
             </a>
           </li>
         </ul>
-      </nav>
-      <div className="relative mt-8 mb-10 flex w-full items-center justify-between md:hidden">
-        <a className="w-[3.755rem]" href="./index.html">
-          <img src={logo} alt="logo" />
-        </a>
-        <button
-          className="absolute top-[0.625rem] right-0 transition-[transform,_opacity] duration-300 ease-in"
-          id="openNav"
-          type="button"
-        >
-          <img src="./images/icons/hamburger.svg" alt="" />
-        </button>
-        <button
-          className="inactive-button absolute top-[0.438rem] right-[0.188rem] transition-[transform,_opacity] duration-300 ease-in"
-          id="closeNav"
-          type="button"
-        >
-          <img src="./images/icons/close.svg" alt="" />
-        </button>
       </div>
-    </div>
+    </nav>
   );
 };
 
