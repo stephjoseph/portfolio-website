91cbe7d3cbf762e5ab9ee7d6ce4f68eb2ea71b56
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-22 15:10:56 +0800

    Create Main.jsx

---
+++ src/Main.jsx
@@ -0,0 +1,11 @@
+import AboutMe from "./AboutMe";
+
+const Main = () => {
+  return (
+    <main className="mb-20 flex w-full max-w-[40rem] flex-col gap-[7.188rem] px-8 md:mb-24 md:max-w-[64rem] md:gap-24 md:px-[2.438rem] xl:mb-[9.375rem] xl:max-w-[96rem] xl:gap-[9.375rem] xl:px-[10.313rem]">
+      <AboutMe />
+    </main>
+  );
+};
+
+export default Main;
