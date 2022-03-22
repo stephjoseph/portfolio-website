4c0de41c90773bd708c41402cb2e29b8daebd9f4
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-22 11:34:11 +0800

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
+import Hero from "./Hero";
 import MobileNav from "./MobileNav";
+import Navbar from "./Navbar";
 
 const Header = () => {
   const [size, setSize] = useState({
@@ -16,8 +18,10 @@ const Header = () => {
   useEffect(() => (window.onresize = updateSize), []);
 
   return (
-    <header className="relative mb-24 w-full max-w-[40rem] px-8 md:max-w-[64rem] md:px-[2.438rem] xl:mb-[9.375rem] xl:max-w-[96rem] xl:px-[10.313rem]">
+    <header className="relative mb-24 h-[1000px] w-full max-w-[40rem] px-8 md:max-w-[64rem] md:px-[2.438rem] xl:mb-[9.375rem] xl:max-w-[96rem] xl:px-[10.313rem]">
       {size.x < 768 && <MobileNav />}
+      {size.x >= 768 && <Navbar />}
+      <Hero />
     </header>
   );
 };
