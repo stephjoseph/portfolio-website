b6f9a1ed2cc07be605c6295ca2128c57e6a12daa
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-22 23:48:22 +0800

    Create scrollToTop.jsx

---
+++ src/components/scrollToTop.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    gsap.to(window, {
+      duration: 1,
+      scrollTo: { y: 0 },
+    });
+  }, [pathname]);
+
+  return null;
+}
