9dfef88c02991888606c1aa55c98d50f9d9bae17
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-22 15:10:52 +0800

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
   useEffect(() => (window.onresize = updateSize), []);
 
   return (
-    <header className="relative mb-24 h-[1000px] w-full max-w-[40rem] px-8 md:max-w-[64rem] md:px-[2.438rem] xl:mb-[9.375rem] xl:max-w-[96rem] xl:px-[10.313rem]">
+    <header className="relative mb-24 w-full max-w-[40rem] px-8 md:max-w-[64rem] md:px-[2.438rem] xl:mb-[9.375rem] xl:max-w-[96rem] xl:px-[10.313rem]">
       {size.x < 768 && <MobileNav />}
       {size.x >= 768 && <Navbar />}
       <Hero />
