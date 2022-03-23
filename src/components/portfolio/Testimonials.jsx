5641e72312df9500f5053f78fbe389398e506605
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-23 13:26:01 +0800

    Update Testimonials.jsx

--- src/components/portfolio/Testimonials.jsx
+++ src/components/portfolio/Testimonials.jsx
@@ -57,7 +57,7 @@ const Testimonials = () => {
         </p>
         <Link
           className="flex h-12 w-[10.938rem] items-center justify-center border border-solid border-[#33323D] text-[0.75rem] font-normal uppercase leading-[0.875rem] tracking-[2px] text-[#33323D] hover:bg-[#33323D] hover:text-white active:bg-[#33323D] active:text-white"
-          to="/portfolio/testimonials"
+          to="/portfolio/testimonials-slider"
         >
           View Project
         </Link>
