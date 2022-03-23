761044248534f56d57141cf115d382473beeba79
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-23 11:18:12 +0800

    Create Background.jsx

---
+++ src/components/detail/tip/Background.jsx
@@ -0,0 +1,20 @@
+const Background = () => {
+  return (
+    <section className="flex flex-col gap-[1.75rem]">
+      <h2 className="font-['Ibarra_Real_Nova'] text-[2rem] font-normal leading-[2.625rem] tracking-[-0.29px] text-[#33323D]">
+        Project Background
+      </h2>
+      <p className="text-[0.938rem] font-normal leading-[1.875rem] tracking-normal text-[#33323D]/80">
+        This project was a front-end challenge from Frontend Mentor. It’s a
+        platform that enables you to practice building websites to a design and
+        project brief. Each challenge includes mobile and desktop designs to
+        show how the website should look at different screen sizes. Creating
+        these projects has helped me refine my workflow and solve real-world
+        coding problems. I’ve learned something new with each project, helping
+        me to improve and adapt my style.
+      </p>
+    </section>
+  );
+};
+
+export default Background;
