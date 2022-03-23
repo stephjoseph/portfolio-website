c6c2cbe8d3b5b3f9ccfc8182f9c61f8929b4627c
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-23 11:18:26 +0800

    Create Tip.jsx

---
+++ src/components/detail/tip/Tip.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Main from "./Main";
+
+const Tip = () => {
+  return (
+    <div>
+      <Header />
+      <Main />
+    </div>
+  );
+};
+
+export default Tip;
