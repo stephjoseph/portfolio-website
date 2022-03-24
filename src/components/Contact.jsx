49a687b076f3d126a79ce305d3c9cf2064072dd5
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-24 10:27:28 +0800

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,16 @@
+import Header from "./contact/Header";
+import Main from "./contact/Main";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div
+      id="contact"
+      className="grid w-full place-items-center bg-[#FAFAFA] font-['Public_Sans']"
+    >
+      <Header />
+      <Main />
+    </div>
+  );
 };
 
 export default Contact;
