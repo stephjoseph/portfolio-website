d70a4d8047de74ca52f547d472f1e742d327dd8a
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-25 01:50:02 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -33,9 +33,6 @@ function App() {
           className="mb-20 flex  w-full max-w-[40rem] overflow-hidden  px-8 md:max-w-[64rem]  md:px-[2.438rem] xl:mb-[9.375rem] xl:max-w-[96rem] xl:px-[10.313rem]"
         >
           <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
             {routes.map((route) =>
               route.path === "/" || route.path === "/portfolio" ? (
                 <Route
