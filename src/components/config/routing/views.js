bf0b916bf677e5df18e21fc488dba72d71087b23
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-24 14:57:11 +0800

    Create views.js

---
+++ src/components/config/routing/views.js
@@ -0,0 +1,37 @@
+import Portfolio from "../../Portfolio";
+import Contact from "../../Contact";
+import Tip from "../../detail/tip/Tip";
+import Officelite from "../../detail/officelite/Officelite";
+import Maker from "../../detail/maker/Maker";
+import Typemaster from "../../detail/typemaster/Typemaster";
+import Github from "../../detail/github/Github";
+import Art from "../../detail/art/Art";
+import Suite from "../../detail/suite/Suite";
+import Time from "../../detail/time/Time";
+import Crowdfunding from "../../detail/crowdfunding/Crowdfunding";
+import Pricing from "../../detail/interactive-pricing/Pricing";
+import Testimonials from "../../detail/testimonials-slider/Testimonials";
+import Equalizer from "../../detail/equalizer/Equalizer";
+import Meet from "../../detail/meet/Meet";
+import Pod from "../../detail/pod/Pod";
+import FAQAccordion from "../../detail/faq-accordion/FAQAccordion";
+
+export default {
+  Portfolio,
+  Contact,
+  Tip,
+  Officelite,
+  Maker,
+  Typemaster,
+  Github,
+  Art,
+  Suite,
+  Time,
+  Crowdfunding,
+  Pricing,
+  Testimonials,
+  Equalizer,
+  Meet,
+  Pod,
+  FAQAccordion,
+};
