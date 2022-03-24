ecb5e8d089284702b5b6590044969ca9004d96a2
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-25 02:41:47 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -33,22 +33,14 @@ function App() {
           className="mb-20 flex  w-full max-w-[40rem] overflow-hidden  px-8 md:max-w-[64rem]  md:px-[2.438rem] xl:mb-[9.375rem] xl:max-w-[96rem] xl:px-[10.313rem]"
         >
           <Switch>
-            {routes.map((route) =>
-              route.path === "/" || route.path === "/portfolio" ? (
-                <Route
-                  exact
-                  key={route.path}
-                  path={route.path}
-                  component={route.component}
-                />
-              ) : (
-                <Route
-                  key={route.path}
-                  path={route.path}
-                  component={route.component}
-                />
-              )
-            )}
+            {routes.map((route) => (
+              <Route
+                exact={true}
+                key={route.path}
+                path={route.path}
+                component={route.component}
+              />
+            ))}
             <Route component={PageNotFound} />
           </Switch>
         </div>
