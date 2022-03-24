f2e9b8698059a68002f0c2ff4c5b459947d4e005
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-24 14:57:10 +0800

    Create routes.js

---
+++ src/components/config/routing/routes.js
@@ -0,0 +1,106 @@
+import views from "./views";
+
+const Portfolio = {
+  component: views.Portfolio,
+  path: "/portfolio",
+};
+
+const Contact = {
+  component: views.Contact,
+  path: "/contact",
+};
+
+const Tip = {
+  component: views.Tip,
+  path: "/portfolio/tip",
+};
+
+const Officelite = {
+  component: views.Officelite,
+  path: "/portfolio/officelite",
+};
+
+const Maker = {
+  component: views.Maker,
+  path: "/portfolio/maker",
+};
+
+const Typemaster = {
+  component: views.Typemaster,
+  path: "/portfolio/typemaster",
+};
+
+const Github = {
+  component: views.Github,
+  path: "/portfolio/github-user-search",
+};
+
+const Art = {
+  component: views.Art,
+  path: "/portfolio/art-gallery",
+};
+
+const Suite = {
+  component: views.Suite,
+  path: "/portfolio/suite",
+};
+
+const Time = {
+  component: views.Time,
+  path: "/portfolio/time-tracking-dashboard",
+};
+
+const Crowdfunding = {
+  component: views.Crowdfunding,
+  path: "/portfolio/crowdfunding",
+};
+
+const Pricing = {
+  component: views.Pricing,
+  path: "/portfolio/interactive-pricing",
+};
+
+const Testimonials = {
+  component: views.Testimonials,
+  path: "/portfolio/testimonials-slider",
+};
+
+const Equalizer = {
+  component: views.Equalizer,
+  path: "/portfolio/equalizer",
+};
+
+const Meet = {
+  component: views.Meet,
+  path: "/portfolio/meet",
+};
+
+const Pod = {
+  component: views.Pod,
+  path: "/portfolio/pod",
+};
+
+const FAQAccordion = {
+  component: views.FAQAccordion,
+  path: "/portfolio/faq-accordion",
+};
+
+export default [
+  Portfolio,
+  Contact,
+  Tip,
+  Officelite,
+  Maker,
+  Typemaster,
+  Github,
+  Art,
+  Suite,
+  Time,
+  Crowdfunding,
+  Pricing,
+  Testimonials,
+  Equalizer,
+  Meet,
+  Pod,
+  FAQAccordion,
+];
