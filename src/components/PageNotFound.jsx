75ff487a23b6d6c74e3eb6b28ee5ccbe80a4a762
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-26 16:46:32 +0800

    Update PageNotFound.jsx

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -4,7 +4,7 @@ const PageNotFound = () => {
   return (
     <div className="grid h-[40vh] w-full place-items-center gap-10 md:h-[72vh] xl:h-[46.75vh]">
       <h1 className="text-center font-['Ibarra_Real_Nova'] text-[3.125rem] leading-[3.125rem] tracking-[-0.45px]">
-        404 - Not Found!
+        404 - Page Not Found
       </h1>
       <Link
         className="flex h-12 w-[10.125rem] items-center justify-center border border-solid border-[#33323D] text-[0.75rem] font-normal uppercase leading-[0.875rem] tracking-[2px] hover:bg-[#33323D] hover:text-white active:bg-[#33323D] active:text-white"
