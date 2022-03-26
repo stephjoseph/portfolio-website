b09044b39111adf0d40475a9348096b17e9777f1
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-26 16:14:16 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,14 +23,14 @@ function App() {
   return (
     <Router>
       <ScrollToTop />
-      <div id="App" className="grid place-items-center bg-[#fafafa]">
+      <div id="App" className="absolute overflow-auto bg-[#fafafa]">
         <div className="relative w-full max-w-[40rem] bg-[#fafafa] px-8 md:max-w-[64rem] md:px-[2.438rem] xl:max-w-[96rem] xl:px-[10.313rem]">
           {size.x < 768 && <MobileNav />}
           {size.x >= 768 && <Navbar />}
         </div>
         <div
           id="content"
-          className="mb-20 flex  w-full max-w-[40rem] overflow-hidden  px-8 md:max-w-[64rem]  md:px-[2.438rem] xl:mb-[9.375rem] xl:max-w-[96rem] xl:px-[10.313rem]"
+          className="mb-20 flex w-full max-w-[40rem] overflow-hidden  px-8 md:max-w-[64rem]  md:px-[2.438rem] xl:mb-[9.375rem] xl:max-w-[96rem] xl:px-[10.313rem]"
         >
           <Switch>
             {routes.map((route) => (
