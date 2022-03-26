ddca058878e7e8ecfc28ba6e2b25c67aa3b6521c
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-03-26 16:30:53 +0800

    Update Animations

--- src/components/portfolio/Crowdfunding.jsx
+++ src/components/portfolio/Crowdfunding.jsx
@@ -24,6 +24,7 @@ const Crowdfunding = () => {
       {
         scrollTrigger: {
           trigger: element,
+          toggleActions: "restart reverse play pause",
         },
         duration: 1.5,
         x: 0,
